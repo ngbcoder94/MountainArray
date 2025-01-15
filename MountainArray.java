ebe4881de1ab70e414b707440eeca7fc60b16fa2
ngbcoder94 <ngbcoder94@gmail.com>
2025-01-14 21:32:54 -0500

    MountainArray class created

---
+++ MountainArray.java
@@ -0,0 +1,16 @@
+/*
+ * This is the class that I will use in order to solve the Leetcode problem: MountainArray.
+ *
+ * Author: Nick Burkett
+ * Date: 01/14/2025
+ *
+ * Status: WIP
+ */
+public class MountainArray{
+
+        //Need to create a main method for testing. 
+        public static void main(String[] args){
+                
+                System.out.println("Hello World");
+        }
+}
